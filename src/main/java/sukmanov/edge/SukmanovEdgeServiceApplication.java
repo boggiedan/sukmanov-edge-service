8a3f0d6b9bb57c5d654bb419c1baea5a1765f864
boggie <sukmanov.b@gmail.com>
2019-12-04 22:02:03 +0100

    initial setup zuul proxy

---
+++ src/main/java/sukmanov/edge/SukmanovEdgeServiceApplication.java
@@ -0,0 +1,17 @@
+package sukmanov.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class SukmanovEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SukmanovEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sukmanov/edge/SukmanovEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sukmanov.edge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukmanovEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
