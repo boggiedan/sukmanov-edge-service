afc8910f905b66bc208f04f590bce1d33a4b8ca8
boggie <sukmanov.b@gmail.com>
2019-12-05 23:33:26 +0100

    add security + changing group id

--- src/main/java/com/sukmanov/edge/security/SecurityConfiguration.java
+++ src/main/java/com/sukmanov/edge/security/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.sukmanov.edge.security;
 
+import com.sukmanov.configuration.security.JwtConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfiguration {
 
-//    @Bean
-//    public sukmanov.commmon.security.JwtConfig JwtConfig() { return new JwtConfig(); }
+    @Bean
+    public JwtConfig JwtConfig() { return new JwtConfig(); }
 }
